68a35f2f06f56b8d755b7eef0a3bc29b88695a68
Nirav Mehta <niravm@ashapurasoftech.com>
2024-06-11 16:46:30 +0530

    Deployed Website

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
-// const path = require('path')
+const path = require('path')
 
-// module.exports = {
-//     distDir: 'build',
-//     sassOptions: {
-//         includePaths: [path.join(__dirname, 'styles')],
-//     },
-//     trailingSlash: true,
-// }
-
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-    distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+module.exports = {
+    distDir: 'build',
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    trailingSlash: true,
+}
