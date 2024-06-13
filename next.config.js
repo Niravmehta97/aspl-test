08868ef2fe2e37de9ff5fbd4960f7a132797787d
Nirav Mehta <niravm@ashapurasoftech.com>
2024-06-13 11:40:03 +0530

    Deployed Website

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
-const path = require('path')
-
-module.exports = {
-    distDir: 'build',
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-    trailingSlash: true,
-}
+// const path = require('path')
 
 // module.exports = {
-//   // other exports
-//   target: 'serverless', // add this line
-// };
+//     distDir: 'build',
+//     sassOptions: {
+//         includePaths: [path.join(__dirname, 'styles')],
+//     },
+//     trailingSlash: true,
+// }
+
+module.exports = {
+  sassOptions: {
+            includePaths: [path.join(__dirname, 'styles')],
+        },
+        trailingSlash: true,
+  // other exports
+  target: 'serverless', // add this line
+};
