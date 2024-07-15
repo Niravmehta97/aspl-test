1e5bfe0178bedab4681677e3237c0b448784117b
Nirav Mehta <niravm@ashapurasoftech.com>
2024-07-15 11:20:02 +0530

    Deployed Website

---
+++ components/ComponentV1/WhyYouNeedV1/Data/Salesforce_Devops_Data.js
@@ -0,0 +1,11 @@
+export const getsalesforceservicecloudwhyData = () => {
+    return {
+        title: 'What Reason Do You Need',
+        span_title: 'Salesforce DevOps Tools?',
+        desc: [
+            `First and foremost, not every person will require Salesforce DevOps tools. In the event that you run a little Org, with a moderately fundamental execution and a little improvement group, you may be content with the local Salesforce devices accessible, for example, change sets.`,
+            `In any case, on the off chance that you have a bigger group, with different designers working across various sandboxes, or perhaps utilizing complex items, for example, Salesforce CPQ, Salesforce DevOps devices may very well become fundamental.`,
+            `There are a couple of sorts of apparatuses which we will cover in the following segment, yet we should investigate how Salesforce change sets work to comprehend the requirement for Salesforce DevOps instruments, particularly with regards to delivering the executives.`
+        ]
+    }
+}
